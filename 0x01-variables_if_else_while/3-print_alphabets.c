ba2b3a9a6a309116cc2b319c4e7fa365739b4e9b
PrudyTK <Prudyytk@gmail.com>
2023-02-17 15:18:43 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Enry point prints the last digit of randomly generated number
+ * and whether it is greater than 5, less than 6, or 0
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	clscr();
+	printf("Print uppercase letters then lowercase letters\n");
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar ('\n');
+	for (ch = 'a'; ch <= 'z' ch++)
+		putchar(ch);
+	putchar('\n');
+	getch();
+}
