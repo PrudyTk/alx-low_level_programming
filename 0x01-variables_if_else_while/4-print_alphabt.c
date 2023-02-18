2c863152dca35479a01052a41f91862070ca53f6
PrudyTK <Prudyytk@gmail.com>
2023-02-17 16:00:23 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+/**
+ * main - prints the alphabets exclude q and e
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		if(ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
