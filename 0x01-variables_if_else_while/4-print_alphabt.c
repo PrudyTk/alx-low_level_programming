0aa50318db2d174f5193859c55eff2d3a6f1e29e
PrudyTK <Prudyytk@gmail.com>
2023-02-17 16:03:41 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,16 +5,28 @@
 */
 int main(void)
 {
+
 	char ch = 'a';
-	
+
+
 	while (ch <= 'z')
+
 	{
-		if(ch != 'e' && ch != 'q')
+
+		if (ch != 'e' && ch != 'q')
+
 		{
+
 			putchar(ch);
+
 		}
+
 		ch++;
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
