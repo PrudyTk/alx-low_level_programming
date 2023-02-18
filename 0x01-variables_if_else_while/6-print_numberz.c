5df932b15635f8675cbf0c39cf28280218002e4f
PrudyTK <Prudyytk@gmail.com>
2023-02-17 16:17:14 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Prints all single digits of bas 10 starting from 0.
+ *followed by a new line
+ *Return: Always 0 (Success)
+*/
+int main(void)
+	{
+
+	int n;
+
+
+	for (n = 48; n < 58; n++)
+
+	{
+
+		putchar(n);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
