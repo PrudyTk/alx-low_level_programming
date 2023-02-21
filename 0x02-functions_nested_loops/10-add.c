cc559b76909e4553316ac92fa3299565dadb6b36
PrudyTK <Prudyytk@gmail.com>
2023-02-21 15:12:54 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * add - Adds two values
+  * @c1: The first number to be computed
+  * @c2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int c1, int c2)
+{
+	return (c1 + c2);
+}
