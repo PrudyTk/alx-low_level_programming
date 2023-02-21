033c2483affd704c691c9c82da74eb81df0183cd
PrudyTK <Prudyytk@gmail.com>
2023-02-21 11:21:47 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
-  *main - Entry point print ALX Student
+  *main - Entry point print _putchar next line
   *Return: Always 0 (Success)
 */
 int main(void)
 {
-	char sh = "ALX Student";
-
-	while (sh)
-	{
-		_putchar(sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
