fef9fba92ab850c2f114960f3b312f733af0df27
PrudyTK <Prudyytk@gmail.com>
2023-02-21 10:10:23 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+*/
+int main(void)
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar(' ');
+	_putchar('S');
+	_putchar('t');
+	_putchar('u');
+	_putchar('d');
+	_putchar('e');
+	_putchar('n');
+	_putchar('t');
+	_putchar('\n');
+
+	return (0);
+}
