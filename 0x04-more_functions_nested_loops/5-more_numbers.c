3e6c5ddbf54d52ae1987ed20aed3cd912bbbaa1f
PrudyTK <Prudyytk@gmail.com>
2023-02-23 11:43:27 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
   * more_numbers - Print 10 times the numbers since 0 up to 14
   *
   * Return: 10 times of the numbers since 0 up to 14
-  */
+* */
 void more_numbers(void)
 {
 	int x, y;
@@ -18,8 +18,6 @@ void more_numbers(void)
 			}
 			_putchar((y % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
 }
-
