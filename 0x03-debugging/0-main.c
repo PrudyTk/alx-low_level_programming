95f56aaaede02846940ea259fa38942a1be12217
PrudyTK <Prudyytk@gmail.com>
2023-02-23 10:05:10 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,10 @@
  *main - tests function that prints if integer is positive or negative
  *Return: 0
 */
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
+	i = 0;
+	positive_or_negative(i);
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	return (0);
 }
