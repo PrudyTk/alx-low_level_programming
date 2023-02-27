5140b1fafc5344c370bdfc66aaf84a0c3637efbd
PrudyTK <Prudyytk@gmail.com>
2023-02-27 10:50:58 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * reset_to_98 - Updates the value it points to 98
+  * @i: pointer to i
+  * Return: void
+*/
+void reset_to_98(int *i)
+{
+	*i = 98;
+}
